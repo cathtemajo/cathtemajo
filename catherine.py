0f4c9e0224794b2e5db66bef4ed2be70587b5f5a
cathtemajo <cathtemajo@gmail.com>
2021-05-24 09:47:40 +0800

    Version 1 by Catherine Temajo

---
+++ catherine.py
@@ -0,0 +1,46 @@
+# print("Welcome to Python")
+# print{"Hi"}
+
+# name = "Ma. Catherine Joy L. Temajo"
+# course = "BSIT"
+# contact = 9269275188
+# email = "ma.catherine.joy.temajo@gmail.com"
+
+# print("Name:  " + str(name))
+# print("Course:  " + str(course))
+# print("Contact:  " + str(contact))
+# print("Email:  " + str(email))
+
+# print("\n")
+# print("     |       |     ")
+# print("  X  |   X   |  O  ")
+# print("-----+-------+-----")
+# print("  O  |   O   |     ")
+# print("-----+-------+-----")
+# print("  X  |       |     ")
+# print("     |       |     ")
+
+
+
+print("     M      M    FFFFFF")
+print("     M M  M M    F     ")
+print("     M   M  M    FFFF  ")
+print("     M      M    F     ")
+print("     M      M    F     ")
+
+
+
+
+
+
+
+
+
+
+# print("XXXXXXXXXXXX '")
+# print("X   _   _  X '")
+# print("X   O   O  X '")
+# print("X     ^    X '")
+# print("X \ \____/  X '")
+# print("X          X '")
+# print("XXXXXXXXXXXX'")
